a4189c19fff7044aa29fc22a0325be0ab0b876f8
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-07-04 19:41:57 +0500

    Add equal operator

--- include/bits.hpp
+++ include/bits.hpp
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 #include <cstring>
+#include <functional>
 
 template<class Value>
 class bits {
@@ -17,7 +18,7 @@ public:
 
 private:
 	template<class To, class From>
-	constexpr static void bit_cast_to(To& to, const From& from) {
+	static constexpr void bit_cast_to(To& to, const From& from) {
 		static_assert(sizeof(To) == sizeof(From));
 		if constexpr (std::is_trivially_assignable_v<To, From>) {
 			to = from;
@@ -26,14 +27,36 @@ private:
 		}
 	}
 	template<class To, class From>
-	constexpr static void bit_cast_to(const To&, const From&) = delete;
+	static constexpr void bit_cast_to(const To&, const From&) = delete;
+
+	template<class Fn, class Left, class Right>
+	static constexpr bool bit_compare(const Left& left, const Right& right) {
+		static_assert(sizeof(Left) == sizeof(Right));
+		if constexpr (std::is_same_v<Left, Right> && std::is_trivial_v<Left>) {
+			return Fn{}(left, right);
+		} else {
+			return Fn{}(std::memcmp(&left, &right, sizeof(Left)), 0);
+		}
+	}
 
 public:
 
 	template<class T>
 	constexpr bits& operator=(const T& other) {
+		static_assert(sizeof(value_type) == sizeof(T));
 		bit_cast_to(m_value, other);
 		return *this;
+	}
+	
+	template<class T>
+	friend constexpr bool operator==(const bits& left, const T& right) {
+		static_assert(sizeof(value_type) == sizeof(T));
+		return bit_compare<std::equal_to<>>(left.m_value, right);
+	}
+	template<class T>
+	friend constexpr bool operator==(const T& left, const bits& right) {
+		static_assert(sizeof(value_type) == sizeof(T));
+		return bit_compare<std::equal_to<>>(left, right.m_value);
 	}
 
 private:
--- test/operators.cpp
+++ test/operators.cpp
@@ -5,11 +5,23 @@ namespace {
 
 struct operators : ::testing::Test {
 	int value = 1;
+	const int const_value = 2;
+	static constexpr int constexpr_value = 3;
 };
 
 TEST_F(operators, assigment) {
 	bits{value} = 2;
 	EXPECT_EQ(value, 2);
+}
+
+TEST_F(operators, equal) {
+	EXPECT_TRUE(bits{value} == 1);
+	EXPECT_TRUE(bits{const_value} == 2);
+	static_assert(bits{constexpr_value} == 3);
+
+	EXPECT_TRUE(1 == bits{value});
+	EXPECT_TRUE(2 == bits{const_value});
+	static_assert(3 == bits{constexpr_value});
 }
 
 }
