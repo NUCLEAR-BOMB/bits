fcdfc8b87a7b6b9dc68be7a54fd1c04730da6cfc
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-07-04 19:12:15 +0500

    Add value method

--- include/bits.hpp
+++ include/bits.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <cstring>
 
 template<class T>
 class bits {
@@ -12,6 +13,17 @@ public:
 	bits(const bits&) = delete;
 	bits& operator=(const bits&) = delete;
 	bits(T&&) = delete;
+
+	constexpr T& value() { return m_value; }
+	constexpr const T& value() const { return m_value; }
+
+private:
+	template<class Dest, class Src>
+	static void bit_copy(Dest* const dest, const Src* const src, const std::size_t count) {
+		std::memcpy(dest, src, count);
+	}
+
+public:
 
 private:
 	T& m_value;
--- test/basic.cpp
+++ test/basic.cpp
@@ -4,15 +4,22 @@
 
 namespace {
 
-TEST(basic, constructor) {
+struct basic : ::testing::Test {
 	int value = 1;
+	const int const_value = 2;
+	static constexpr int constexpr_value = 3;
+};
+
+TEST_F(basic, constructor) {
 	(void)bits{value};
+	(void)bits{const_value};
+	static_assert(((void)bits{constexpr_value}, true));
+}
 
-	const int cvalue = 2;
-	(void)bits{cvalue};
-
-	constexpr int cevalue = 3;
-	static_assert(((void)bits{cevalue}, true));
+TEST_F(basic, value_method) {
+	EXPECT_EQ(bits{value}.value(), value);
+	EXPECT_EQ(bits{const_value}.value(), const_value);
+	static_assert(bits{constexpr_value}.value() == constexpr_value);
 }
 
 }
